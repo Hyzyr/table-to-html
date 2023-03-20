cd0c3ab8f82f7665f45f97f548e2fea586c53bef
Hyzyr <avezberdiyew@gmail.com>
2023-03-21 02:54:12 +0500

    popup

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -15,22 +15,11 @@ if (menu && menuBtn) {
   };
 }
 
-// const popup = document.getElementById("popup");
-// const popupBtn = document.getElementById("popupBtn");
-// const popupClose = document.getElementById("popupClose");
+const howMenu = document.getElementById("howMenu");
+const howMenuBtn = document.getElementById("howMenuBtn");
 
-// const togglePopup = () => {
-//   document.body.classList.toggle("active");
-//   popup.classList.toggle("active");
-//   popupBtn.classList.toggle("active");
-// };
-// popupBtn.onclick = () => togglePopup();
-// popupClose.onclick = () => togglePopup();
-// popup.onclick = (e) => {
-//   if (e.currentTarget === e.srcElement) {
-//     togglePopup();
-//   }
-// };
+if (howMenu && howMenuBtn)
+  howMenuBtn.onclick = () => howMenu.classList?.toggle("active");
 
 const tabBtnFunc = (btn) => {
   btn.onclick = () => {
