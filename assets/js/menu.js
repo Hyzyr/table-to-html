1e559d2b91ce0a586e727182dac1c6261d614c27
Hyzyr <avezberdiyew@gmail.com>
2023-03-21 02:51:09 +0500

    popup func

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,17 +1,19 @@
 const menu = document.getElementById("menu");
 const menuBtn = document.getElementById("menuBtn");
 
-const toggleMenu = () => {
-  document.body.classList.toggle("active");
-  menu.classList.toggle("active");
-  menuBtn.classList.toggle("active");
-};
-menuBtn.onclick = () => toggleMenu();
-menu.onclick = (e) => {
-  if (e.currentTarget === e.srcElement) {
-    toggleMenu();
-  }
-};
+if (menu && menuBtn) {
+  const toggleMenu = () => {
+    document.body.classList.toggle("active");
+    menu.classList.toggle("active");
+    menuBtn.classList.toggle("active");
+  };
+  menuBtn.onclick = () => toggleMenu();
+  menu.onclick = (e) => {
+    if (e.currentTarget === e.srcElement) {
+      toggleMenu();
+    }
+  };
+}
 
 // const popup = document.getElementById("popup");
 // const popupBtn = document.getElementById("popupBtn");
@@ -29,3 +31,23 @@ menu.onclick = (e) => {
 //     togglePopup();
 //   }
 // };
+
+const tabBtnFunc = (btn) => {
+  btn.onclick = () => {
+    const parentSelector = btn.getAttribute("data-parent");
+    const parent = document.querySelector(parentSelector);
+    btn.classList.add("active");
+    parent.querySelectorAll('[data-event="tabBtn"]').forEach((tabBtn) => {
+      if (btn !== tabBtn) {
+        tabBtn.classList.remove("active");
+      }
+    });
+  };
+};
+
+const eventBtns = document.querySelectorAll("[data-event]");
+eventBtns.forEach((btn) => {
+  if (btn.getAttribute("data-event") === "tabBtn") {
+    tabBtnFunc(btn);
+  }
+});
